e6c51a521533f7ac2a0e322631615313e1ef8b29
Paul-Vismonte <paulvismonte0@gmail.com>
2025-02-20 14:26:23 +0800

    profile

--- app/src/main/java/com/example/barberhub/ProfileActivity.kt
+++ app/src/main/java/com/example/barberhub/ProfileActivity.kt
@@ -1,20 +1,25 @@
 package com.example.barberhub
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Handle Back Button Click
+        findViewById<View>(R.id.btnBack).setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
+            finish() // Close ProfileActivity
+        }
+
+        findViewById<View>(R.id.btnSettings).setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+            finish() // Close ProfileActivity
         }
     }
 }
